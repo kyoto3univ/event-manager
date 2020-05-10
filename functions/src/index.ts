59597b51f5bd830d6658ca183365a523139d5158
f0reachARR <f0reach@f0reach.me>
2020-05-10 22:41:29 +0900

    Remove test function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,15 +1,7 @@
 import * as admin from 'firebase-admin';
-import * as functions from 'firebase-functions';
 
 admin.initializeApp({
   credential: admin.credential.applicationDefault(),
-});
-
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-export const helloWorld = functions.https.onRequest((_request, response) => {
-  response.send('Hello from Firebase!');
 });
 
 export * from './users';
