19715f39b66df7fb46c438d5da44c3b7f6773587
f0reachARR <f0reach@f0reach.me>
2020-05-10 22:40:46 +0900

    Set region

--- functions/src/users.ts
+++ functions/src/users.ts
@@ -1,8 +1,8 @@
 import * as admin from 'firebase-admin';
-import { firestore } from 'firebase-functions';
+import { region } from 'firebase-functions';
 
-export const setUserRole = firestore
-  .document('/users/{userId}')
+export const setUserRole = region('asia-northeast1')
+  .firestore.document('/users/{userId}')
   .onWrite(async (snapshot, context) => {
     const { userId } = context.params;
     await admin.auth().setCustomUserClaims(
