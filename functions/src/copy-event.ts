0012bca6a2b5e8fff66468590e1dcf4ec50cd548
f0reachARR <f0reach@f0reach.me>
2020-05-12 21:38:08 +0900

    Type check for copy event

--- functions/src/copy-event.ts
+++ functions/src/copy-event.ts
@@ -1,19 +1,21 @@
 import { database } from 'firebase-admin';
 import { region } from 'firebase-functions';
-
+import { isRight } from 'fp-ts/lib/Either';
+import { eventDocumentIo } from './models/Event';
 export const copyEvent = region('asia-northeast1')
   .firestore.document('/events/{eventId}')
   .onWrite(async (change) => {
-    if (change.after) {
-      const docData = change.after.data();
-      const dbEntity = database().ref('events').child(change.after.id);
-      if (docData) {
-        await dbEntity.update(docData);
+    const dbEntity = database().ref('events').child(change.after.id);
+
+    if (change.after.exists) {
+      const docData = eventDocumentIo.decode(change.after.data());
+
+      if (isRight(docData) && docData.right.published) {
+        await dbEntity.set(docData.right);
       } else {
         await dbEntity.remove();
       }
     } else {
-      const dbEntity = database().ref('events').child(change.before.id);
       await dbEntity.remove();
     }
   });
