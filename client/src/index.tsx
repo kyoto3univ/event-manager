3493d3ee9a2248135009e4355bbd0e360567a674
f0reachARR <f0reach@f0reach.me>
2020-06-18 21:42:35 +0900

    Add initial files for SPA

---
+++ client/src/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+const main = document.querySelector('main');
+if (main) {
+  render(<div />, main);
+}
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,104 +3,116 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackHarddiskPlugin = require('html-webpack-harddisk-plugin');
+const firebaseTools = require('firebase-tools');
 
 // eslint-disable-next-line no-console
 console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
 
-const config = isProd => ({
-  context: path.resolve(__dirname, './src'),
+const config = async isProd => {
+  const apps = await firebaseTools.apps.list('WEB', {});
+  const currentApp = apps[0];
+  const __FIREBASE_CONFIG__ = await firebaseTools.apps.sdkconfig(
+    'WEB',
+    currentApp.appId,
+    {},
+  );
 
-  entry: {
-    app: './index.tsx',
-  },
+  return {
+    context: path.resolve(__dirname, './src'),
 
-  output: {
-    filename: isProd ? '[name]-[hash].js' : '[name].js',
-    chunkFilename: isProd ? '[name]-[hash].js' : '[name].js',
-    path: path.resolve(__dirname, '../public'),
-    publicPath: '/',
-  },
+    entry: {
+      app: './index.tsx',
+    },
 
-  optimization: {
-    splitChunks: {
-      cacheGroups: {
-        vendor: {
-          test: /[\\/]node_modules[\\/](react|firebase)/,
-          name: 'vendor',
-          chunks: 'all',
+    output: {
+      filename: isProd ? '[name]-[hash].js' : '[name].js',
+      chunkFilename: isProd ? '[name]-[hash].js' : '[name].js',
+      path: path.resolve(__dirname, '../public'),
+      publicPath: '/',
+    },
+
+    optimization: {
+      splitChunks: {
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/](react|firebase)/,
+            name: 'vendor',
+            chunks: 'all',
+          },
         },
       },
     },
-  },
 
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        exclude: /node_modules/,
-        use: 'babel-loader',
-      },
-      {
-        test: /\.(jpe?g|png|gif|ttf|otf|eot|svg|woff2?)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: isProd ? '[name]-[hash].[ext]' : '[name].[ext]',
-              publicPath: '/',
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          exclude: /node_modules/,
+          use: 'babel-loader',
+        },
+        {
+          test: /\.(jpe?g|png|gif|ttf|otf|eot|svg|woff2?)$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: isProd ? '[name]-[hash].[ext]' : '[name].[ext]',
+                publicPath: '/',
+              },
             },
-          },
-        ],
-      },
-      {
-        test: /\.css$/,
-        use: [
-          !isProd ? 'style-loader' : MiniCssExtractPlugin.loader,
-          'css-loader',
-        ],
-      },
-    ],
-  },
-
-  devtool: 'source-map',
-  resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx'],
-  },
-
-  plugins: [
-    isProd ? null : new webpack.HotModuleReplacementPlugin(),
-
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
-    }),
-
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: './index.html',
-      minify: isProd,
-      alwaysWriteToDisk: true,
-    }),
-
-    new HtmlWebpackHarddiskPlugin(),
-
-    new MiniCssExtractPlugin({
-      filename: isProd ? '[name].[hash].css' : '[name].css',
-      chunkFilename: isProd ? '[id].[hash].css' : '[id].css',
-    }),
-  ].filter(plugin => !!plugin),
-
-  devServer: {
-    compress: true,
-    contentBase: path.join(__dirname, '../public'),
-    disableHostCheck: true,
-    historyApiFallback: true,
-    hot: true,
-    port: 8000,
-    watchOptions: {
-      ignored: /node_modules/,
+          ],
+        },
+        {
+          test: /\.css$/,
+          use: [
+            !isProd ? 'style-loader' : MiniCssExtractPlugin.loader,
+            'css-loader',
+          ],
+        },
+      ],
     },
-    host: '0.0.0.0',
-  },
-});
+
+    devtool: 'source-map',
+    resolve: {
+      extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    },
+
+    plugins: [
+      isProd ? null : new webpack.HotModuleReplacementPlugin(),
+
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+        __FIREBASE_CONFIG__,
+      }),
+
+      new HtmlWebpackPlugin({
+        filename: 'index.html',
+        template: './index.html',
+        minify: isProd,
+        alwaysWriteToDisk: true,
+      }),
+
+      new HtmlWebpackHarddiskPlugin(),
+
+      new MiniCssExtractPlugin({
+        filename: isProd ? '[name].[hash].css' : '[name].css',
+        chunkFilename: isProd ? '[id].[hash].css' : '[id].css',
+      }),
+    ].filter(plugin => !!plugin),
+
+    devServer: {
+      compress: true,
+      contentBase: path.join(__dirname, '../public'),
+      disableHostCheck: true,
+      historyApiFallback: true,
+      hot: true,
+      port: 8000,
+      watchOptions: {
+        ignored: /node_modules/,
+      },
+      host: '0.0.0.0',
+    },
+  };
+};
 
 module.exports = (_command, { mode }) => config(mode === 'production');
