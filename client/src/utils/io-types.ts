e8175062036e0eb5d00aaf641b01158587caf337
f0reachARR <f0reach@f0reach.me>
2020-06-24 14:43:41 +0900

    Fix firestore converter

--- client/src/utils/io-types.ts
+++ client/src/utils/io-types.ts
@@ -1,22 +1,23 @@
-import { FirestoreDataConverter, Timestamp } from '@google-cloud/firestore';
+import { firestore } from 'firebase/app';
 import { isRight } from 'fp-ts/lib/Either';
 import * as t from 'io-ts';
 
-export const firestoreTimestamp = new t.Type<Timestamp>(
+export const firestoreTimestamp = new t.Type<firestore.Timestamp>(
   'Timestamp',
-  (ts): ts is Timestamp => ts instanceof Timestamp,
-  (ts, ctx) => (ts instanceof Timestamp ? t.success(ts) : t.failure(ts, ctx)),
+  (ts): ts is firestore.Timestamp => ts instanceof firestore.Timestamp,
+  (ts, ctx) =>
+    ts instanceof firestore.Timestamp ? t.success(ts) : t.failure(ts, ctx),
   t.identity,
 );
 
 export const ioTsConverter = <C extends t.Any>(
   type: C,
-): FirestoreDataConverter<C['_A']> => ({
+): firestore.FirestoreDataConverter<C['_A']> => ({
   toFirestore(doc) {
     return type.encode(doc);
   },
-  fromFirestore(data) {
-    const decoded = type.decode(data);
+  fromFirestore(snapshot) {
+    const decoded = type.decode(snapshot.data());
     if (isRight(decoded)) return decoded.right;
     throw decoded.left;
   },
